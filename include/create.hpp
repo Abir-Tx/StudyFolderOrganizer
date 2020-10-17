5a86674862922bfdb61541d43445d3ab1efad519
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-10-17 22:43:20 +0600

    Cretae Term folders creation with auto and manual

--- include/create.hpp
+++ include/create.hpp
@@ -10,6 +10,9 @@ std::string semester;
 int subjectNumber;
 std::vector<std::string> subjects;
 std::string subjectName;
+std::vector<std::string> selectedFoldersName;
+int selectedFoldersNumber;
+bool isManualCreationChoice = false;
 
 void createFolder()
 {
@@ -17,6 +20,51 @@ void createFolder()
     std::cin >> semester;
     std::cout << "How Many Subjects have you got: ";
     std::cin >> subjectNumber;
+
+
+
+    std::cout << "Do you want to manually specify folders under terms sections?" << std::endl;
+    std::string yesNoOption = {"Yes", "No"};
+    for (int i = 0; i < yesNoOption.size(); i++)
+    {
+        std::cout << (i + 1) << ". " << yesNoOption[i] << std::endl;
+    }
+    int choice;
+    std::cin >> choice;
+    if (choice == 1)
+    {
+        manualCreationChoice = true;
+    }
+    else if (choice == 2)
+    {
+        isManualCreationChoice = false;
+    }
+    else
+    {
+        std::cout << "Bad input. Going with automatic creation" << std::endl;
+        isManualCreationChoice = true;
+    }
+
+    if (isManualCreationChoice == true)
+    {
+        std::cout << "How many folders you wanna create inside the Term Folders (Lessons Excluded): ";
+        std::cin >> selectedFoldersNumber;
+        for (int i = 0; i < selectedFolderNumber; i++)
+        {
+            std::cout << "Enter name of folder no: " << (i + 1);
+            std::cin >> selectedFoldersName;
+            subjects.push_back(selectedFoldersName);
+        }
+    }
+    else
+    {
+        std::cout << "Going with automatic creation";
+        std::cout << std::endl;
+        //Preloading the lessons folder
+        selectedFolders.push_back("Lessons");
+    }
+
+
 
     for (int i = 0; i < subjectNumber; i++)
     {
@@ -38,9 +86,32 @@ void createFolder()
     for (int i = 0; i < subjects.size(); i++)
     {
         std::string subjectPath = "D:\\University Study\\Study Materials\\1st Year\\" +
-                                  semester + "th Semester\\" + subjects[i] ; 
+                                  semester + "th Semester\\" + subjects[i];
 
         _mkdir(subjectPath.c_str());
+    }
+
+    //Term creation
+    for (int i = 0; i < subjects.size(); i++)
+    {
+        std::string midTermPath = "D:\\University Study\\Study Materials\\1st Year\\" +
+                                  semester + "th Semester\\" + subjects[i] + "\\Mid-Term";
+        _mkdir(midTermPath.c_str());
+    }
+    for (int i = 0; i < subjects.size(); i++)
+    {
+        std::string finalTermPath = "D:\\University Study\\Study Materials\\1st Year\\" +
+                                    semester + "th Semester\\" + subjects[i] + "\\Final";
+        _mkdir(finalTermPath.c_str());
+    }
+
+    //Inside Term Folders Creation
+
+    for (int i = 0; i < selectedFoldersNumber; i++)
+    {
+        std::string midTermPath = "D:\\University Study\\Study Materials\\1st Year\\" +
+                                  semester + "th Semester\\" + subjects[i] + "\\Mid-Term\\" + selectedFolders[i];
+        _mkdir(midTermPath.c_str());
     }
 }
 #endif // CREATE_H
