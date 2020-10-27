1344be89461362db30cd5a68dd455bed74e5bbc0
Restyled.io <commits@restyled.io>
2020-10-27 19:46:57 +0000

    Restyled by clang-format

--- include/delete/delete.hpp
+++ include/delete/delete.hpp
@@ -1,57 +1,44 @@
-#if !defined (DELTE_H)
+#if !defined(DELTE_H)
 #define DELTE_H
-#include <iostream>
-#include <iomanip>
 #include <direct.h>
+#include <iomanip>
+#include <iostream>
 
 void deleteCreated() {
-    system("cls");
-    std::cout<<"\t\t\t\t\tDelete Mode";
-    std::cout<<std::endl;
+  system("cls");
+  std::cout << "\t\t\t\t\tDelete Mode";
+  std::cout << std::endl;
 
-    std::cout<<"Are you sure you want to delete the whole directory including files?";
-    std::cout<<std::endl;
-    bool isSure;
-    std::string options[2] = {"Yes", "No"};
-    for (int i = 0; i < 2; i++)
-    {
-        std::cout<<(i+1)<<". "<<options[i];
-        std::cout<<std::endl;
+  std::cout
+      << "Are you sure you want to delete the whole directory including files?";
+  std::cout << std::endl;
+  bool isSure;
+  std::string options[2] = {"Yes", "No"};
+  for (int i = 0; i < 2; i++) {
+    std::cout << (i + 1) << ". " << options[i];
+    std::cout << std::endl;
+  }
+  std::cout << "Your choice: ";
+  int choice;
+  std::cin >> choice;
+  std::cout << std::endl;
 
-    }
-    std::cout<<"Your choice: ";
-    int choice;
-    std::cin>>choice;
-    std::cout<<std::endl;
+  if (choice == 1) {
+    isSure = true;
+  } else {
+    isSure = false;
+  }
 
-    if (choice == 1) {
-        isSure = true;
-    }
-    else
-    {
-        isSure = false;
-    }
+  // Main script calling
+  if (isSure) {
+    system("wscript ..\\include\\delete\\deleterLauncher.vbs");
+  } else if (isSure == false) {
+    std::cout << "Deletion cancelled. Nothing deleted !";
+    std::cout << std::endl;
 
-
-    // Main script calling
-    if (isSure)
-    {
-        system("wscript ..\\include\\delete\\deleterLauncher.vbs");
-    }
-    else if (isSure == false)
-    {
-        std::cout<<"Deletion cancelled. Nothing deleted !";
-        std::cout<<std::endl;
-
-    }
-    else
-    {
-        std::cout<<"Inavlid Error Occured";
-        std::cout<<std::endl;
-    }
-
-
-
-
+  } else {
+    std::cout << "Inavlid Error Occured";
+    std::cout << std::endl;
+  }
 }
 #endif // DELTE_H
--- include/home.hpp
+++ include/home.hpp
@@ -1,79 +1,73 @@
 #if !defined(HOME_H)
 #define HOME_H
 
-#include <iostream>
-#include <vector>
+#include "../include/dirTree.hpp"
 #include <direct.h>
 #include <iomanip>
-#include "../include/dirTree.hpp"
+#include <iostream>
+#include <vector>
 
-//User Defined Headers
+// User Defined Headers
+#include "../include/abut.hpp"
 #include "../include/create.hpp"
+#include "../include/delete/delete.hpp"
 #include "../include/driveLetterTaker.hpp"
 #include "../include/rootDirCreator.hpp"
-#include "../include/abut.hpp"
-#include "../include/delete/delete.hpp"
 
-void homeDesign()
-{
-    system("color 04");
-    system("title Study Folder Organizer");
-    std::string title = "Study Folder Organizer";
-    std::cout << std::setw(80) << std::setfill(' ') << title << std::endl
-              << std::endl;
+void homeDesign() {
+  system("color 04");
+  system("title Study Folder Organizer");
+  std::string title = "Study Folder Organizer";
+  std::cout << std::setw(80) << std::setfill(' ') << title << std::endl
+            << std::endl;
 }
 
-void home()
-{
+void home() {
+  system("cls");
+  homeDesign();
+
+  std::cout << "Your Options: " << std::endl;
+
+  std::vector<std::string> homeOptions = {
+      "Create Mode", "About", "Directory Tree", "Delete Mode", "Exit"};
+
+  for (int i = 0; i < homeOptions.size(); i++) {
+    std::cout << (i + 1) << ". " << homeOptions[i] << std::endl;
+  }
+  std::cout << "Your choice: ";
+  int choice;
+  std::cin >> choice;
+  std::cout << std::endl;
+
+  switch (choice) {
+  case 1: {
     system("cls");
-    homeDesign();
-
-    std::cout << "Your Options: " << std::endl;
-
-    std::vector<std::string> homeOptions = {"Create Mode", "About", "Directory Tree", "Delete Mode", "Exit"};
-
-    for (int i = 0; i < homeOptions.size(); i++)
-    {
-        std::cout << (i + 1) << ". " << homeOptions[i] << std::endl;
-    }
-    std::cout << "Your choice: ";
-    int choice;
-    std::cin >> choice;
+    std::cout << std::setw(80) << std::setfill(' ') << "Create Mode";
     std::cout << std::endl;
 
-    switch (choice)
-    {
-    case 1:
-    {
-        system("cls");
-        std::cout << std::setw(80) << std::setfill(' ') << "Create Mode";
-        std::cout << std::endl;
-
-
-        driveTaker();
-        rootDirCreator();
-        createFolder(driveLetter);
-    }
+    driveTaker();
+    rootDirCreator();
+    createFolder(driveLetter);
+  } break;
+  case 2:
+    about();
     break;
-    case 2:
-        about();
-        break;
-    case 3:
-        dirTree();
-        break;
-    case 4:
-        deleteCreated();
-        break;
-    case 5:
-        system("cls");
-        system("color 04");
-        std::cout<<"Exitin the program.......";
-        exit(0);
-        break;
-    default:
-        std::cout << "Invalid";
-        break;
-    }
+  case 3:
+    dirTree();
+    break;
+  case 4:
+    deleteCreated();
+    break;
+  case 5:
+    system("cls");
+    system("color 04");
+    std::cout << "Exitin the program.......";
+    exit(0);
+    break;
+  default:
+    std::cout << "Invalid";
+    break;
+  }
 }
 
 #endif // HOME_H
