651bafbb4cde549472d367b8ac879af06088f0dd
Restyled.io <commits@restyled.io>
2020-10-28 06:34:49 +0000

    Restyled by clang-format

--- include/home.hpp
+++ include/home.hpp
@@ -15,61 +15,59 @@
 #include "../include/rootDirCreator.hpp"
 
 void homeDesign() {
-    system("color 04");
-    system("title Study Folder Organizer");
-    std::string title = "Study Folder Organizer";
-    std::cout << std::setw(80) << std::setfill(' ') << title << std::endl
-              << std::endl;
+  system("color 04");
+  system("title Study Folder Organizer");
+  std::string title = "Study Folder Organizer";
+  std::cout << std::setw(80) << std::setfill(' ') << title << std::endl
+            << std::endl;
 }
 
 void home() {
+  system("cls");
+  homeDesign();
+
+  std::cout << "Your Options: " << std::endl;
+
+  std::vector<std::string> homeOptions = {
+      "Create Mode", "About", "Directory Tree", "Delete Mode", "Exit"};
+
+  for (int i = 0; i < homeOptions.size(); i++) {
+    std::cout << (i + 1) << ". " << homeOptions[i] << std::endl;
+  }
+  std::cout << "Your choice: ";
+  int choice;
+  std::cin >> choice;
+  std::cout << std::endl;
+
+  switch (choice) {
+  case 1: {
     system("cls");
-    homeDesign();
-
-    std::cout << "Your Options: " << std::endl;
-
-    std::vector<std::string> homeOptions = {
-        "Create Mode", "About", "Directory Tree", "Delete Mode", "Exit"
-    };
-
-    for (int i = 0; i < homeOptions.size(); i++) {
-        std::cout << (i + 1) << ". " << homeOptions[i] << std::endl;
-    }
-    std::cout << "Your choice: ";
-    int choice;
-    std::cin >> choice;
+    std::cout << std::setw(80) << std::setfill(' ') << "Create Mode";
     std::cout << std::endl;
 
-    switch (choice) {
-    case 1: {
-        system("cls");
-        std::cout << std::setw(80) << std::setfill(' ') << "Create Mode";
-        std::cout << std::endl;
-
-        driveTaker();
-        rootDirCreator();
-        createFolder(driveLetter);
-    }
+    driveTaker();
+    rootDirCreator();
+    createFolder(driveLetter);
+  } break;
+  case 2:
+    about();
     break;
-    case 2:
-        about();
-        break;
-    case 3:
-        dirTree();
-        break;
-    case 4:
-        deleteCreated();
-        break;
-    case 5:
-        system("cls");
-        system("color 04");
-        std::cout << "Exitin the program.......";
-        exit(0);
-        break;
-    default:
-        std::cout << "Invalid";
-        break;
-    }
+  case 3:
+    dirTree();
+    break;
+  case 4:
+    deleteCreated();
+    break;
+  case 5:
+    system("cls");
+    system("color 04");
+    std::cout << "Exitin the program.......";
+    exit(0);
+    break;
+  default:
+    std::cout << "Invalid";
+    break;
+  }
 }
 
 #endif // HOME_H
