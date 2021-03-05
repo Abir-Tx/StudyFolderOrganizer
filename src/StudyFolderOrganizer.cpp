28369647d64477829edd4fb7551d5e82cede6cba
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2021-03-06 02:09:14 +0600

    Format with clang format style

--- src/StudyFolderOrganizer.cpp
+++ src/StudyFolderOrganizer.cpp
@@ -1,24 +1,23 @@
-/* A C++ source code file by Mushfiqur Rahman Abir. This terminal app is for windows only. I am making this for personal usage case. 
-Anyone can modify this codes to meet their needs if they like the idea of the software */
+/* A C++ source code file by Mushfiqur Rahman Abir. This terminal app is for
+windows only. I am making this for personal usage case.
+Anyone can modify this codes to meet their needs if they like the idea of the
+software */
 
 #include <iostream>
 #include "../include/home.hpp"
 
-int main()
-{
+int main() {
     int choice;
-    do
-    {
+    do {
         home();
-        std::cout<<std::endl;
+        std::cout << std::endl;
         std::cout << "Press 1 to go to homepage again & others to Exit: ";
         std::cin >> choice;
     } while (choice == 1);
-    if(choice != 1)
-    {
+    if (choice != 1) {
         system("cls");
         system("color 04");
-        std::cout<<"Exitin the program.......";
+        std::cout << "Exitin the program.......";
         exit(0);
     }
 }
