4734fb8ceda98a17dba2919929de1cd92ae596af
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-06-26 18:59:06 +0600

    Replace old title with new `decorateMe` of `ALib` `v1.0.0`

--- include/about.hpp
+++ include/about.hpp
@@ -1,21 +1,21 @@
-#if !defined (ABOUT_H)
+#if !defined(ABOUT_H)
 #define ABOUT_H
-#include <iostream>
+#include "alib.hpp"
 #include <iomanip>
+#include <iostream>
 
-void about()
-{
+void about() {
     system("cls");
-    std::cout<<std::setw(80)<<std::setfill(' ')<<"About SFO"<<std::endl<<std::endl;
-    std::cout<<std::endl;
-    
+    alib::decorateMe("About SFO", 1, "-", true);
 
-    std::cout<<"SFO stands for Study Folder Organizer. Developed & managed by Mushfiqur Rahman Abir. "<<
-    "This small tool allows to manage a student's university's digital files in a organized way."<<
-    "This app creates a structured directory to specified drive for managing and keeping files";
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-
+    std::cout << "SFO stands for Study Folder Organizer. Developed & managed "
+                 "by Mushfiqur Rahman Abir. "
+              << "This small tool allows to manage a student's university's "
+                 "digital files in a organized way."
+              << "This app creates a structured directory to specified drive "
+                 "for managing and keeping files";
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
 }
 #endif // ABOUT_H
