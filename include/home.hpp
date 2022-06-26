366041123541c221c292d74b90894cefac6d9202
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-06-26 18:43:48 +0600

    Fix `ALib` inclusion

--- include/home.hpp
+++ include/home.hpp
@@ -1,7 +1,7 @@
 #if !defined(HOME_H)
 #define HOME_H
 
-#include "../include/dirTree.hpp"
+#include "alib.hpp"
 #include <direct.h>
 #include <iomanip>
 #include <iostream>
@@ -11,64 +11,64 @@
 #include "../include/abut.hpp"
 #include "../include/create.hpp"
 #include "../include/delete/delete.hpp"
+#include "../include/dirTree.hpp"
 #include "../include/driveLetterTaker.hpp"
 #include "../include/rootDirCreator.hpp"
-#include "alib.hpp"
 
 void homeDesign() {
-  system("color 04");
-  system("title Study Folder Organizer");
-  std::string title = "Study Folder Organizer";
+    system("color 04");
+    system("title Study Folder Organizer");
+    std::string title = "Study Folder Organizer";
 
-  alib::straightLineDecor("Study Folder Organizer", 2);
+    alib::decorateMe("Study Folder Organizer", 2, "_", true);
 }
 
 void home() {
-  system("cls");
-  homeDesign();
-
-  std::cout << "Your Options: " << std::endl;
-
-  std::vector<std::string> homeOptions = {
-      "Create Mode", "About", "Directory Tree", "Delete Mode", "Exit"};
-
-  for (int i = 0; i < homeOptions.size(); i++) {
-    std::cout << (i + 1) << ". " << homeOptions[i] << std::endl;
-  }
-  std::cout << "Your choice: ";
-  int choice;
-  std::cin >> choice;
-  std::cout << std::endl;
-
-  switch (choice) {
-  case 1: {
     system("cls");
-    std::cout << std::setw(80) << std::setfill(' ') << "Create Mode";
+    homeDesign();
+
+    std::cout << "Your Options: " << std::endl;
+
+    std::vector<std::string> homeOptions = {
+        "Create Mode", "About", "Directory Tree", "Delete Mode", "Exit"};
+
+    for (int i = 0; i < homeOptions.size(); i++) {
+        std::cout << (i + 1) << ". " << homeOptions[i] << std::endl;
+    }
+    std::cout << "Your choice: ";
+    int choice;
+    std::cin >> choice;
     std::cout << std::endl;
 
-    driveTaker();
-    rootDirCreator();
-    createFolder(driveLetter);
-  } break;
-  case 2:
-    about();
-    break;
-  case 3:
-    dirTree();
-    break;
-  case 4:
-    deleteCreated();
-    break;
-  case 5:
-    system("cls");
-    system("color 04");
-    std::cout << "Exitin the program.......";
-    exit(0);
-    break;
-  default:
-    std::cout << "Invalid";
-    break;
-  }
+    switch (choice) {
+    case 1: {
+        system("cls");
+        std::cout << std::setw(80) << std::setfill(' ') << "Create Mode";
+        std::cout << std::endl;
+
+        driveTaker();
+        rootDirCreator();
+        createFolder(driveLetter);
+    } break;
+    case 2:
+        about();
+        break;
+    case 3:
+        dirTree();
+        break;
+    case 4:
+        deleteCreated();
+        break;
+    case 5:
+        system("cls");
+        system("color 04");
+        std::cout << "Exitin the program.......";
+        exit(0);
+        break;
+    default:
+        std::cout << "Invalid";
+        break;
+    }
 }
 
 #endif // HOME_H
